7681b860f65db3507986d0e9dfb98a53bd9edca6
Alando Appling <alandoappling@gmail.com>
2019-04-05 19:12:09 -0700

    Added Authentication, completed register and login routes, and got jokes!

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'add a .env file to root of project with the JWT_SECRET variable';
 
+const Users = require('../users/users=model.js');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -10,11 +16,52 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(e => {
+      res.status(500).json(e)
+    })
 }
 
 function login(req, res) {
   // implement user login
+  let {username, password} = req.body;
+  Users.findBy({ username })
+    .first()
+    .then( user => {
+      if(user && bcrypt.compareSync(password, user.password)){
+        const token = generateToken(user);
+        res.status(200).json({
+          token,
+          message: `You now have access to the worlds best jokes ${user.username}`,
+          user
+        })
+      }else{
+        res.status(401).json({message: 'Invalid Credentials'})
+      }
+    })
+
+  function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+
+    const options = {
+        expiresIn: "1d"
+    }
+
+    return jwt.sign(payload, jwtKey, options)
+  }
 }
+
+
 
 function getJokes(req, res) {
   const requestOptions = {
